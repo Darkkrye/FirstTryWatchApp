4b80ecf789fa0ac817053b4014edbc7008c534b7
Openfield Mobility <pboudon@opnf.fr>
2016-09-29 11:04:04 +0200

    Initial Commit

---
+++ WatchKitComplicationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKitComplicationTest
+//
+//  Created by Openfield Mobility on 29/09/2016.
+//  Copyright © 2016 Openfield Mobility. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
